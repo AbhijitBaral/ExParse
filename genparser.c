6119ff5d72ce16fef8bb9efb3f730f4a4ba79275
AbhijitBaral <vaishnavo1355@gmail.com>
2024-08-23 15:40:08 +0530

    File has to be written frm scratch and not edites

--- genparser.c
+++ genparser.c
@@ -3,22 +3,22 @@
 double parse(char *input){
    //Does something and writes the expression into the parser file containing a single function(eval) that returns the evaluated result. This result is then returned to the caller function by this "parse" function
 
-    token *tokenizedList, *crntToken;
-    tokenizedList=lex(input);
-    crntToken=tokenizedList;
+  token *tokenizedList, *crntToken;
+  tokenizedList=lex(input);
+  crntToken=tokenizedList;
+  char fileName[]="parser.c";
+  FILE *file=fopen(fileName,"w");
+  if(file==NULL){
+    perror("Could not open file for editing");
+    return;
+  }
 
-    FILE *file=fopen(parser.c);
-    if(file==NULL){
-      perror("Could not open file for editing");
-      return;
-    }
-    
+  fprintf(file,"#include <stdio.h>\n#include <stdlib.h>\n#include <math.h>\n\ndouble eval(void){\n\tdouble val=0.0;\n\t/");
 
-    while(crntToken!=NULL){
-
-        crntToken=crntToken->next;
-    }
+  while(crntToken!=NULL){ 
+      crntToken=crntToken->next;
+  }
 
 
-   return(eval);
+  return(eval);
 } 
